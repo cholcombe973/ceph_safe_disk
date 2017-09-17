e34ad6d58e5825c64f17cf6e200a205af65f3876
Chris Holcombe <xfactor973@gmail.com>
2017-09-17 13:37:26 -0700

    Deserialize owned

--- lib/from.rs
+++ lib/from.rs
@@ -8,14 +8,14 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::fmt::Debug;
 
-use self::serde::de::Deserialize;
+use self::serde::de::DeserializeOwned;
 
 // Generic trait to read file to serializable struct
 pub trait FromFile<T> {
     fn from_file(path: &str) -> Result<T, CSDError>;
 }
 
-impl<'de, T: Deserialize<'de> + Debug> FromFile<T> for T {
+impl<T: DeserializeOwned + Debug> FromFile<T> for T {
     fn from_file(path: &str) -> Result<T, CSDError> {
         let mut file = try!(File::open(path));
         let mut buffer = String::new();
@@ -31,7 +31,7 @@ pub trait FromCeph<T> {
     fn from_ceph(cmd: &str) -> Result<T, CSDError>;
 }
 
-impl<'de, T: Deserialize<'de> + Debug> FromCeph<T> for T {
+impl<T: DeserializeOwned + Debug> FromCeph<T> for T {
     fn from_ceph(cmd: &str) -> Result<T, CSDError> {
         Ok(try!(serde_json::from_str(&try!(call_ceph(cmd)))))
     }
